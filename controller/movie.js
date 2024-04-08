6ddad1bb29f0161966614737b883df338c8b5126
Chethan N <43665442+chethanswap@users.noreply.github.com>
2024-04-08 09:13:03 +0530

    Update movie.js

--- controller/movie.js
+++ controller/movie.js
@@ -6,12 +6,7 @@ import Genre from "../models/genre.js";
 
 import { upload } from "../utils/cloudinary.js";
 
-/**
- * Read all movies.
- * @route GET /api/movies
- * @returns {object} An object containing the count and list of movies.
- * @throws {Error} If an error occurs while retrieving the movies.
- */
+
 router.get("/", async (req, res) => {
   try {
     const movies = await Movie.find().populate("genre", "name").exec();
@@ -24,13 +19,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-/**
- * Read a movie by its ID.
- * @route GET /api/movies/:movieId
- * @param {string} movieId - The ID of the movie to retrieve.
- * @returns {object} The movie object.
- * @throws {Error} If the movie is not found or an error occurs while retrieving it.
- */
+
 router.get("/:movieId", async (req, res) => {
   try {
     const movie = await Movie.findById({ _id: req.params.movieId })
@@ -45,19 +34,7 @@ router.get("/:movieId", async (req, res) => {
   }
 });
 
-/**
- * Add a new movie.
- * @route POST /api/movies/addMovie
- * @param {string} title - The title of the movie.
- * @param {string} genre - The genre of the movie.
- * @param {number} rate - The rating of the movie.
- * @param {string} description - The description of the movie.
- * @param {string} trailerLink - The link to the movie's trailer.
- * @param {number} movieLength - The length of the movie in minutes.
- * @param {File} image - The image file for the movie.
- * @returns {object} A success message if the movie is added successfully.
- * @throws {Error} If the movie already exists, an error occurs while saving the movie, or validation fails.
- */
+
 router.post("/addMovie", upload.single("image"), async (req, res) => {
   try {
     const { title, genre, rate, description, trailerLink, movieLength } =
@@ -96,13 +73,7 @@ router.post("/addMovie", upload.single("image"), async (req, res) => {
   }
 });
 
-/**
- * Update a movie by its ID.
- * @route PATCH /api/movies/:movieId
- * @param {string} movieId - The ID of the movie to update.
- * @returns {object} A success message and the updated movie object.
- * @throws {Error} If the movie is not found, an error occurs while updating it, or validation fails.
- */
+
 router.patch("/:movieId", async (req, res) => {
   try {
     const updateMovie = await Movie.findByIdAndUpdate(
